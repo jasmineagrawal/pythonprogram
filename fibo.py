9dd216138ea1badaf00a09fe3c14847c30e202f5
jasmineagrawal <53893949+jasmineagrawal@users.noreply.github.com>
2019-08-22 12:54:34 +0530

    Add files via upload

---
+++ fibo.py
@@ -0,0 +1,23 @@
+def fibo(n):
+    a=1
+    b=0
+    print("0")
+    print("1")
+
+    while n!=0:
+        c=a
+        a=a+b
+        b=c
+        n=n-1
+        print(a)
+
+
+n=int(input("enter the number of seq"))
+if n==0:
+    print("0")
+elif n==1:
+    print("1")
+elif n==2:
+    print("1")
+else:
+    fibo(n)
